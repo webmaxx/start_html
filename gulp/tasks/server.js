c8e0518f332c70ee14abd38c91bc58d4f1b532e5
webmaxx <webmaxx@webmaxx.name>
2017-12-11 16:05:29 +0300

    Move gulp watchers from server task to watch task

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -4,12 +4,5 @@ import config from '../config'
 
 gulp.task('server', () => {
   browserSync(config.server)
-
-  gulp.watch(`${config.src.js}/**/*${config.src.jsExt}`, ['webpack:watch'])
-  gulp.watch(`${config.src.sass}/**/*.${config.src.sassExt}`, ['sass:watch'])
-  gulp.watch(`${config.src.templates}/**/*${config.src.templatesExt}`, ['templates:watch'])
-  gulp.watch(`${config.src.images}/**/*`, ['images:watch'])
-  gulp.watch(`${config.src.fonts}/**/*`, ['fonts:watch'])
-
   return gulp
 })

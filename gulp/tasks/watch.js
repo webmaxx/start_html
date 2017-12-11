6f2f87f9259c86b54adfb66de1d4f76e052d48dd
webmaxx <webmaxx@webmaxx.name>
2017-12-11 23:01:43 +0300

    Fix gulp watcher for sass task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@ import config from '../config'
 
 gulp.task('watch', ['build', 'server'], () => {
     gulp.watch(`${config.src.js}/**/*${config.src.jsExt}`, ['webpack:watch'])
-    gulp.watch(`${config.src.sass}/**/*.${config.src.sassExt}`, ['sass:watch'])
+    gulp.watch(`${config.src.sass}/**/*${config.src.sassExt}`, ['sass:watch'])
     gulp.watch(`${config.src.templates}/**/*${config.src.templatesExt}`, ['templates:watch'])
     gulp.watch(`${config.src.images}/**/*`, ['images:watch'])
     gulp.watch(`${config.src.fonts}/**/*`, ['fonts:watch'])
